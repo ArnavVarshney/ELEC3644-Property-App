e72dc17e88838cb17684bb3979239a22d9e4a545
mattmak <makyilam@gmail.com>
2024-10-09 17:39:16 +0800

    Matt testing

--- ELEC3644 Property App/ContentView.swift
+++ ELEC3644 Property App/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("texting")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
