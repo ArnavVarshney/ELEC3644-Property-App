2507c5b87b00f00607ffbc71b8812ff9acc30b6a
Filbert Tejalaksana <filberttejalaksana@Filberts-Mac-mini.local>
2024-10-09 14:11:12 +0800

    init

---
+++ ELEC3644 Property App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ELEC3644 Property App
+//
+//  Created by Filbert Tejalaksana on 9/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ELEC3644 Property App/ELEC3644_Property_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELEC3644_Property_AppApp.swift
+//  ELEC3644 Property App
+//
+//  Created by Filbert Tejalaksana on 9/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ELEC3644_Property_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
