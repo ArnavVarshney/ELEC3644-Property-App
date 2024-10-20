cc4eed60cfbaa3601761922a05e1f4bfba88a87f
Arnav Varshney <varshney.arnav@gmail.com>
2024-10-21 03:39:25 +0800

    Update PropertyCardView

---
+++ ELEC3644 Property/Extensions/ImageCarouselView.swift
@@ -0,0 +1,32 @@
+//
+//  ImageCarouselView.swift
+//  ELEC3644 Property
+//
+//  Created by Arnav Varshney on 21/10/24.
+//
+
+
+import SwiftUI
+
+struct ImageCarouselView: View {
+    let images: [String]
+    let height: Double = 320
+    var cornerRadius: Double = 10
+
+    var body: some View {
+        TabView {
+            ForEach(images, id: \.self) { image in
+                Image(image)
+                    .resizable()
+                    .scaledToFill()
+            }
+        }
+        .frame(height: height)
+        .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+        .tabViewStyle(.page)
+    }
+}
+
+#Preview {
+    ImageCarouselView(images: mockPropertyImages)
+}
--- ELEC3644 Property/Views/Explore/PropertyCardView.swift
+++ ELEC3644 Property/Views/Explore/PropertyCardView.swift
@@ -7,44 +7,40 @@
 
 import SwiftUI
 
+let mockPropertyImages = [
+    "Property1",
+    "Property2",
+    "Property3",
+    "Property4",
+    "Property5",
+
+]
+
 struct PropertyCardView: View {
     let property: Property
 
     var body: some View {
-        VStack {
-            TabView {
-                ForEach(1 ..< 6) { index in
-                    Image("Property\(index)")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 328, height: 310)
-                        .scaledToFit()
-                        .cornerRadius(12)
-                        .clipped()
-                }
-            }
-            .tabViewStyle(PageTabViewStyle())
-            .frame(width: .infinity, height: 310)
-            .padding(.bottom, 4)
+        VStack(spacing: 8) {
+            ImageCarouselView(images: mockPropertyImages)
 
             HStack(alignment: .top) {
                 VStack(alignment: .leading) {
                     Text(property.name)
-                        .font(.system(size: 14, weight: .medium))
-                        .foregroundColor(.neutral100)
-                        .padding(.bottom, 1)
+                        .fontWeight(.bold)
+
                     Text("\(property.saleableArea)sqft")
-                        .font(.system(size: 14, weight: .regular))
-                        .foregroundColor(.neutral70)
+                        .foregroundColor(.gray)
+                    
                     Text("\(property.subDistrict), \(property.area)")
-                        .font(.system(size: 14, weight: .regular))
-                        .foregroundColor(.neutral70)
-                        .padding(.bottom, 1)
+                        .foregroundColor(.gray)
+                    
                     Text("HKD \(property.netPrice)")
-                        .font(.system(size: 14, weight: .medium))
-                        .foregroundColor(.neutral100)
+                        .foregroundColor(.gray)
+                        .fontWeight(.semibold)
                 }
+                
                 Spacer()
+                
                 VStack(alignment: .trailing) {
                     ForEach(property.facilities) { facility in
                         HStack {
@@ -52,16 +48,16 @@ struct PropertyCardView: View {
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 24, height: 24)
+                            
                             Text("\(facility.measure) \(facility.measureUnit)")
-                                .font(.system(size: 14, weight: .medium))
-                                .foregroundColor(.neutral100)
+                                .fontWeight(.semibold)
                         }
                     }
                 }
             }
-            .padding(.horizontal, 36)
-            .padding(.bottom, 12)
+            .font(.footnote)
         }
+        .padding(.horizontal)
     }
 }
 
