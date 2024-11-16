ceda9612e7bd02c48ddb9e717c71c7719c7f7840
Arnav Varshney <varshney.arnav@gmail.com>
2024-11-17 01:36:04 +0800

    inbox view when logged out

--- ELEC3644 Property/Views/Inbox/InboxView.swift
+++ ELEC3644 Property/Views/Inbox/InboxView.swift
@@ -12,28 +12,46 @@ struct InboxView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                if inboxData.chats.isEmpty {
-                    Image(systemName: "bubble.left.and.text.bubble.right")
-                        .font(.largeTitle)
-                        .padding()
-                    Text("You don't have any messages")
-                        .font(.footnote)
-                        .fontWeight(.bold)
-                        .padding(4)
-                    Text("When you receive a new message, it will appear here.")
-                        .font(.footnote)
-                        .foregroundColor(.neutral60)
-                        .padding(4)
-                } else {
-                    List(inboxData.chats) { chat in
-                        NavigationLink {
-                            ChatView(chat: chat)
-                                .toolbar(.hidden, for: .tabBar)
-                        } label: {
-                            InboxItemView(user: chat.user, message: chat.messages.last!)
+                if userViewModel.isLoggedIn() {
+                    if inboxData.chats.isEmpty {
+                        Image(systemName: "bubble.left.and.text.bubble.right")
+                            .font(.largeTitle)
+                            .padding()
+                        Text("You don't have any messages")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .padding(4)
+                        Text("When you receive a new message, it will appear here.")
+                            .font(.footnote)
+                            .foregroundColor(.neutral60)
+                            .padding(4)
+                    } else {
+                        List(inboxData.chats) { chat in
+                            NavigationLink {
+                                ChatView(chat: chat)
+                                    .toolbar(.hidden, for: .tabBar)
+                            } label: {
+                                InboxItemView(user: chat.user, message: chat.messages.last!)
+                            }
                         }
+                        .listStyle(InsetListStyle())
                     }
-                    .listStyle(InsetListStyle())
+                } else {
+                    VStack(alignment: .leading) {
+                        Text("Log in to see messages")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.top, 24)
+                        Text("Once you login, you'll find messages here.")
+                            .font(.footnote)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.top, 4)
+                            .padding(.trailing, 8)
+                        LoginButton()
+                        Spacer()
+                    }
+                    .padding(.horizontal, 32)
                 }
             }
             .background(.neutral10)
