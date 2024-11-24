0084f18909abb08ffd9de4e4dc75b7f549fe9a7e
Arnav Varshney <varshney.arnav@gmail.com>
2024-11-24 19:59:31 +0800

    fix switch to hosting icon position

--- ELEC3644 Property/Views/Profile/ProfileView.swift
+++ ELEC3644 Property/Views/Profile/ProfileView.swift
@@ -21,126 +21,133 @@ struct ProfileView: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView(showsIndicators: false) {
+            ZStack {
                 if userViewModel.isLoggedIn() {
-                    NavigationLink(destination: ProfileDetailedView(user: user)) {
-                        HStack(spacing: 18) {
-                            UserAvatarView(user: user, size: 64)
-                                .addShadow()
-                            VStack(alignment: .leading) {
-                                Text(user.name)
-                                    .font(.headline)
-                                Text("Show profile")
-                                    .font(.footnote)
-                                    .foregroundColor(.neutral70)
+                    ScrollView(showsIndicators: false) {
+                        NavigationLink(destination: ProfileDetailedView(user: user)) {
+                            HStack(spacing: 18) {
+                                UserAvatarView(user: user, size: 64)
+                                    .addShadow()
+                                VStack(alignment: .leading) {
+                                    Text(user.name)
+                                        .font(.headline)
+                                    Text("Show profile")
+                                        .font(.footnote)
+                                        .foregroundColor(.neutral70)
+                                }
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 18, height: 18)
+                                    .foregroundColor(.black)
                             }
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 18, height: 18)
-                                .foregroundColor(.black)
                         }
-                    }
 
-                    NavigationLink(destination: HostTransitionScreen()) {
-                        HStack(spacing: 24) {
-                            VStack(spacing: 12) {
-                                Text("List your property")
-                                    .font(.headline)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .fontWeight(.semibold)
-                                Text("It's easy to start hosting and earn extra income")
-                                    .font(.caption)
-                                    .foregroundColor(.neutral70)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
+                        NavigationLink(destination: HostTransitionScreen()) {
+                            HStack(spacing: 24) {
+                                VStack(spacing: 12) {
+                                    Text("List your property")
+                                        .font(.headline)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .fontWeight(.semibold)
+                                    Text("It's easy to start hosting and earn extra income")
+                                        .font(.caption)
+                                        .foregroundColor(.neutral70)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                .padding(.leading, 24)
+                                .padding(.vertical, 24)
+
+                                Image("hosting_advert")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 96, height: 96)
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 12)
                             }
-                            .padding(.leading, 24)
-                            .padding(.vertical, 24)
-
-                            Image("hosting_advert")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 96, height: 96)
-                                .foregroundColor(.black)
-                                .padding(.trailing, 12)
-                        }
-                        .background(.white)
-                        .cornerRadius(12)
-                        .addShadow()
-                        .padding(.vertical, 24)
-                    }
-                    .buttonStyle(PlainButtonStyle())
-
-                    Text("Settings")
-                        .font(.headline)
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.top, 12)
-                    SettingsList(items: [
-                        SettingsItem(
-                            destination: AnyView(GeneralSettingsView()), iconName: "gearshape",
-                            title: "General", subtitle: ""),
-                        SettingsItem(
-                            destination: AnyView(PersonalInfoView()),
-                            iconName: "person.crop.circle",
-                            title: "Personal Information", subtitle: ""),
-                        SettingsItem(
-                            destination: AnyView(LoginSecurityView()),
-                            iconName: "shield.righthalf.filled", title: "Login & Security",
-                            subtitle: ""),
-                        SettingsItem(
-                            destination: AnyView(AccessibilityView()), iconName: "accessibility",
-                            title: "Accessibility", subtitle: ""),
-                    ])
-                    Button {
-                        showLogoutConfirmation = true
-                    } label: {
-                        Text("Log out")
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                            .underline()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.top, 24)
-                    }
-                    .alert(isPresented: $showLogoutConfirmation) {
-                        Alert(
-                            title: Text("Confirm Logout"),
-                            message: Text("Are you sure you want to log out?"),
-                            primaryButton: .destructive(Text("Log out")) {
-                                userViewModel.logout()
-                            },
-                            secondaryButton: .cancel()
-                        )
-                    }
-                    Spacer()
-                    NavigationLink(
-                        destination: HostTransitionScreen().toolbar(.hidden, for: .tabBar)
-                    ) {
-                        HStack {
-                            Image(systemName: "link")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 24, height: 24)
-                                .foregroundColor(.white)
-                                .symbolEffect(.variableColor)
-                                .padding(.leading, 16)
-                            Text(
-                                userViewModel.userRole == .host
-                                    ? "Switch to exploring" : "Switch to hosting"
-                            )
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 16)
-                            .padding(.trailing, 16)
+                            .background(.white)
+                            .cornerRadius(12)
                             .addShadow()
+                            .padding(.vertical, 24)
                         }
-                        .padding(.horizontal, 4)
-                        .background(.black)
-                        .cornerRadius(36)
-                        .padding(.vertical, 24)
-                    }
+                        .buttonStyle(PlainButtonStyle())
 
+                        Text("Settings")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.top, 12)
+                        SettingsList(items: [
+                            SettingsItem(
+                                destination: AnyView(GeneralSettingsView()), iconName: "gearshape",
+                                title: "General", subtitle: ""),
+                            SettingsItem(
+                                destination: AnyView(PersonalInfoView()),
+                                iconName: "person.crop.circle",
+                                title: "Personal Information", subtitle: ""),
+                            SettingsItem(
+                                destination: AnyView(LoginSecurityView()),
+                                iconName: "shield.righthalf.filled", title: "Login & Security",
+                                subtitle: ""),
+                            SettingsItem(
+                                destination: AnyView(AccessibilityView()),
+                                iconName: "accessibility",
+                                title: "Accessibility", subtitle: ""),
+                        ])
+                        Button {
+                            showLogoutConfirmation = true
+                        } label: {
+                            Text("Log out")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                                .underline()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.top, 24)
+                        }
+                        .alert(isPresented: $showLogoutConfirmation) {
+                            Alert(
+                                title: Text("Confirm Logout"),
+                                message: Text("Are you sure you want to log out?"),
+                                primaryButton: .destructive(Text("Log out")) {
+                                    userViewModel.logout()
+                                },
+                                secondaryButton: .cancel()
+                            )
+                        }
+                    }
+                    .refreshable {
+                        userViewModel.initTask()
+                    }
+                    VStack {
+                        Spacer()
+                        NavigationLink(
+                            destination: HostTransitionScreen().toolbar(.hidden, for: .tabBar)
+                        ) {
+                            HStack {
+                                Image(systemName: "link")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(.white)
+                                    .symbolEffect(.variableColor)
+                                    .padding(.leading, 16)
+                                Text(
+                                    userViewModel.userRole == .host
+                                        ? "Switch to exploring" : "Switch to hosting"
+                                )
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 16)
+                                .padding(.trailing, 16)
+                                .addShadow()
+                            }
+                            .padding(.horizontal, 4)
+                            .background(.black)
+                            .cornerRadius(36)
+                            .padding(.vertical, 24)
+                        }
+                    }
                 } else {
                     VStack(alignment: .leading) {
                         Text("Login to start exploring. ")
@@ -182,9 +189,6 @@ struct ProfileView: View {
                     }
                     .sheet(isPresented: $showModal) { SignupView().presentationDetents([.large]) }
                 }
-            }
-            .refreshable {
-                userViewModel.initTask()
             }
             .padding(.horizontal, 18)
             .navigationTitle("Profile")
