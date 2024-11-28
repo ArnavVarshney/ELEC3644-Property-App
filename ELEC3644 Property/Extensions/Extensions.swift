85ae8f03635f2437e504b49d5accd809c5651117
Filbert Tejalaksana <filberttejalaksana@Filberts-Mac-mini.local>
2024-11-28 11:37:20 +0800

    fix

--- ELEC3644 Property/Extensions/Extensions.swift
+++ ELEC3644 Property/Extensions/Extensions.swift
@@ -98,6 +98,11 @@ extension String {
         }
         return result
     }
+    
+    var isNumber: Bool {
+        let characters = CharacterSet.decimalDigits
+        return CharacterSet(charactersIn: self).isSubset(of: characters)
+    }
 }
 
 extension Data {
